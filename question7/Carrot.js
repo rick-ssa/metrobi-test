ca67b439d88a629f43adfaad336e26678a1c0991
rick-ssa <rick_ssa@hotmail.com>
2022-03-04 15:15:28 -0300

    bug fixed

--- question7/Carrot.js
+++ question7/Carrot.js
@@ -5,50 +5,77 @@ maximum value the bag can hold. [Python or Javascript]*/
 
 
 const getMaxValue = (carrotType, capacity) => {
-    let maxValue = 0
+
+    let maxValue = 0 
 
     // sort for price/kg desc
     carrotType.sort((a,b)=> {
+
         const priceByKgA = a.price / a.kg
-        const priceByKgB = b.price / b.kg
+
+        const priceByKgB = b.price / b.kg 
 
         if(priceByKgA.toFixed(2) === priceByKgB.toFixed(2)) { // case have the same price/kilo sort for kg asc
             return a.kg - b.kg
         }
 
-        return priceByKgB - priceByKgA 
+        return priceByKgB - priceByKgA
+
     })
 
+ 
+
     carrotType.map((carrot,index, array)=>{
+
         let idStart = index + 1
         let quantity = Math.floor(capacity / carrot.kg)
         let value = quantity * carrot.price
         let capacityLeft = capacity - (quantity * carrot.kg)
+
         if (capacityLeft===0) {
             maxValue = Math.max(maxValue,value)
             return
-        }
+        } 
+
         for(let i = idStart; i < array.length; i++ ) {
+
             if(array[i].kg > capacityLeft) {
+                let pullOutOffBagQuantity = (
+                    Math.floor(array[i].kg / carrot.kg) * carrot.kg + capacityLeft >= array[i].kg ?
+                    Math.floor(array[i].kg / carrot.kg) :
+                    Math.floor(array[i].kg / carrot.kg) + 1
+                )
+
+                maxValue = Math.max(maxValue,value)
+                maxValue = Math.max(maxValue,value - pullOutOffBagQuantity * carrot.price + array[i].price)
+
                 continue
             }
+
             quantity = Math.floor(capacityLeft / array[i].kg)
             value += quantity * array[i].price
             capacityLeft = capacityLeft - (quantity * array[i].kg)
+
             if (capacityLeft===0) {
                 maxValue = Math.max(maxValue,value)
                 return
             }
+
         }
 
         maxValue = Math.max(maxValue,value)
+
     })
 
     return maxValue
+
 }
 
-const solution  = getMaxValue( [{kg: 7, price: 150},{kg:2, price:40}, {kg: 37, price: 816},{kg: 5, price: 100},{kg: 3, price: 70}],36)
+const arr = [{kg: 7, price: 150},{kg: 5, price: 100},{kg: 3, price: 70}]
+const capacity = 40
+const solution  = getMaxValue( arr,capacity)
 
-
-
+ 
+console.log(arr)
+console.log(capacity)
 console.log(solution)
